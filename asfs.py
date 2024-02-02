4e6349330c555e73282b8cefd354da96f84d2789
grig <ovcharovgrigory@yandex.ru>
2024-02-02 18:54:56 +1100

    Прикольная функция txt

--- asfs.py
+++ asfs.py
@@ -1,2 +1,13 @@
-def func():
-    pass
+import pygame
+
+cols = 20
+margin = 100
+tile_size = 40
+screen_width = tile_size * cols
+screen_height = (tile_size * cols) + margin
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+def txt(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
