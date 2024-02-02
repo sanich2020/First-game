b02a526593d038f0c640e90b7e5f8099e81d62a9
Andrey <andrey.biryukov07@mail.ru>
2024-02-02 14:19:46 +1100

    + class Button

--- main.py
+++ main.py
@@ -11,6 +11,32 @@ pygame.display.set_caption('Platformer')
 sun_img = pygame.image.load('images/sun.png')
 bg_img = pygame.image.load('images/sky.png')
 
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self):
+        action = False
+
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                action = True
+                self.clicked = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        screen.blit(self.image, self.rect)
+        return action
+
+
 run = True
 while run:
     screen.blit(bg_img, (0, 0))
