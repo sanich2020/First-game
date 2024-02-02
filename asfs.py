d6a822881d94c049d9d8a927f5d7e149557c5418
greg <ovcharovgrigory@yandex.ru>
2024-02-02 23:41:46 +1100

    Класс Exit для перехода на следующий левл

--- asfs.py
+++ asfs.py
@@ -54,3 +54,13 @@ class Coin(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+
+class Exit(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('images/exit.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
